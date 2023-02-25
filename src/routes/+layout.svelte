bbf63491a0e9ecf9534fde8e6eb00b0b6173840b
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-25 21:49:29 +0100

    update root layout & page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,20 @@
 <script lang="ts">
-    import "../app.css"
+	import '../app.css'
+	import db from '$lib/db'
+	import { invalidate } from '$app/navigation'
+	import { onMount } from 'svelte'
+
+	onMount(() => {
+		const {
+			data: { subscription }
+		} = db.auth.onAuthStateChange(() => {
+			invalidate('supabase:auth')
+		})
+
+		return () => {
+			subscription.unsubscribe()
+		}
+	})
 </script>
+
+<slot />
