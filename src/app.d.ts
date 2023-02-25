c1b275e1a4a353eb4dfad6c9bcfb8c62f0992992
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-25 21:48:23 +0100

    setup db client

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,11 +2,18 @@
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Supabase {
+			// Use the path to where you generated the types using the Supbase CLI.
+			Database: import('../types/supabase').Database
+			SchemaName: 'public'
+		}
 		// interface Error {}
 		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			session: import('@supabase/supabase-js').Session | null
+		}
 		// interface Platform {}
 	}
 }
 
-export {};
+export {}
---
+++ src/lib/db.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/auth-helpers-sveltekit'
+import { env } from '$env/dynamic/public'
+// or use the static env
+// import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+
+const db = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+
+export default db
