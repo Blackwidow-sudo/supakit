693a68b754539159a576a62d2242b6a01303095b
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-27 20:43:42 +0100

    configure spa mode & disable ssr on root layout

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,6 +1,9 @@
 import type { LayoutLoad } from './$types'
 import { getSupabase } from '@supabase/auth-helpers-sveltekit'
 
+// Disable SSR for SPA mode
+export const ssr = false
+
 export const load: LayoutLoad = async event => {
 	const { session } = await getSupabase(event)
 	return { session }
