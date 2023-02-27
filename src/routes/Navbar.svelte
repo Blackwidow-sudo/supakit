e7cf8d4e44202b4ad158a7041ae745ea88986399
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-27 20:22:47 +0100

    impl simple Navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { Route } from '../types/app'
+
+	/**
+	 * Props
+	 */
+	export let routes: Route[]
+</script>
+
+<nav class="bg-gradient-to-r from-violet-500 to-fuchsia-500">
+	<ul class="flex flex-row gap-1 items-center justify-center">
+		{#each routes as route (route.name)}
+			<li>
+				<slot {route}>
+					<a
+						class="inline-block text-white px-4 py-2 hover:bg-black/10"
+						href={route.href}>{route.name}</a>
+				</slot>
+			</li>
+		{/each}
+	</ul>
+</nav>
