71d820921b93c1bd80e077f0db23248750e873bc
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-27 20:33:58 +0100

    update root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,3 +11,18 @@
 		<p>I am logged in!</p>
 	{/if}
 </PageTransition>
+<svelte:head>
+	<title>Supakit</title>
+	<meta
+		name="description"
+		content="SvelteKit using Supabase" />
+</svelte:head>
+
+<PageTransition>
+	{#if !$page.data.session}
+		<h1>I am not logged in</h1>
+	{:else}
+		<h1>Welcome {$page.data.session.user.email}</h1>
+		<p>I am logged in!</p>
+	{/if}
+</PageTransition>
