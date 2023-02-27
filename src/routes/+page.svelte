3d2de1c0e3387351ebaaf6f54977533fe1dcf77b
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-27 20:29:50 +0100

    use PageTransition in root page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,13 @@
 <script>
 	import { page } from '$app/stores'
+	import PageTransition from '$lib/components/PageTransition.svelte'
 </script>
 
-{#if !$page.data.session}
-	<h1>I am not logged in</h1>
-{:else}
-	<h1>Welcome {$page.data.session.user.email}</h1>
-	<p>I am logged in!</p>
-{/if}
+<PageTransition>
+	{#if !$page.data.session}
+		<h1>I am not logged in</h1>
+	{:else}
+		<h1>Welcome {$page.data.session.user.email}</h1>
+		<p>I am logged in!</p>
+	{/if}
+</PageTransition>
