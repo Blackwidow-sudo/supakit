21ab2b0dc1c6b4b56c5427ad0e1faf5f0df02b9c
Merlin Cornehl <mcornehl@mkw.eu>
2023-02-27 20:35:37 +0100

    fix bug from merge

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,6 @@
 	import PageTransition from '$lib/components/PageTransition.svelte'
 </script>
 
-<PageTransition>
-	{#if !$page.data.session}
-		<h1>I am not logged in</h1>
-	{:else}
-		<h1>Welcome {$page.data.session.user.email}</h1>
-		<p>I am logged in!</p>
-	{/if}
-</PageTransition>
 <svelte:head>
 	<title>Supakit</title>
 	<meta
